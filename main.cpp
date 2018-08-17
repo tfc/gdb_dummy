dd732f630cb3b37552d62164c5d6d813b8fea861
Jacek Galowicz <jacek@galowicz.de>
2018-08-17 17:45:56 +0200

    Make it talk with GDB! (read/write memory successfully)

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 
 static int global_fd {0};
 
+static std::vector<uint8_t> mem (0x1000, 0);
+
 static uint8_t gdb_checksum(const std::string &s) {
     return std::accumulate(std::begin(s), std::end(s), static_cast<uint8_t>(0));
 }
@@ -91,9 +93,83 @@ static parser<bool> q_messages(buffer_pos &pos) {
             )(pos);
 }
 
+static parser<bool> get_register(buffer_pos &pos) {
+  return map(prefixed(oneOf('p'), integer),
+      [](const auto &x) {
+        std::cout << "GET REGISTER " << x << '\n';
+        send_msg("08ab0000");
+        return true;
+      })(pos);
+}
+
+static std::optional<std::string> read_memory_at(size_t offset, size_t bytes) {
+  if (offset + bytes < mem.size()) {
+    std::ostringstream ss;
+    for (size_t i{offset}; i < offset + bytes; ++i) {
+      ss << std::hex << std::setfill('0') << std::setw(2) << unsigned(mem[i]);
+    }
+    return ss.str();
+  }
+  return {};
+}
+
+static parser<bool> read_memory(buffer_pos &pos) {
+  return map(tuple_of(prefixed(oneOf('m'), base_integer(16)),
+                      prefixed(oneOf(','), base_integer(16))),
+      [] (const auto &x) {
+        const auto offset {std::get<0>(x)};
+        const auto bytes {std::get<1>(x)};
+        if (const auto retmem {read_memory_at(offset, bytes)}) {
+          send_msg(*retmem);
+        } else {
+          send_msg("E00");
+        }
+        return true;
+      })(pos);
+}
+
+static uint8_t fix_hex(uint8_t d) {
+  if ('a' <= d && d <= 'f') { return d - 'a' + 10; }
+  return d - '0';
+}
+
+static parser<uint8_t> byte(buffer_pos &pos) {
+  return map(tuple_of(hexnumber, hexnumber),
+    [] (const auto &t) {
+      const unsigned lo (fix_hex(std::get<1>(t)));
+      const unsigned hi (fix_hex(std::get<0>(t)));
+      return lo | (hi << 4);
+    })(pos);
+}
+
+static parser<bool> write_memory(buffer_pos &pos) {
+  return map(tuple_of(prefixed(oneOf('M'), base_integer(16)),
+                      prefixed(oneOf(','), base_integer(16)),
+                      prefixed(oneOf(':'), manyV(byte))),
+      [] (const auto &x) {
+        const auto offset {std::get<0>(x)};
+        const auto bytes {std::get<1>(x)};
+        const auto content_bytes {std::get<2>(x)};
+
+        if (offset + bytes < mem.size()) {
+          std::copy(std::begin(content_bytes), std::end(content_bytes),
+                    std::begin(mem) + offset);
+          send_msg("OK");
+        } else {
+          send_msg("E00");
+        }
+
+        return true;
+      })(pos);
+}
+
 static parser<bool> parse_gdb_message(buffer_pos &pos) {
     return choice(simple_answer_on("?", "S05"),
                   simple_answer_on("!", "OK"),
+                  simple_answer_on("g", "xxxxxxxx00000000xxxxxxxx00000000"),
+                  get_register,
+                  read_memory,
+                  write_memory,
                   q_messages
             )(pos);
 }
@@ -116,7 +192,7 @@ int main()
         }
 
         while (auto chk_ret {checksum_parser(read_pos)}) {
-            std::cout << "<- [" << *chk_ret << "]\n";
+            //std::cout << "<- [" << *chk_ret << "]\n";
             send_ack();
             str_pos payload_pos {*chk_ret};
             if (auto ret {parse_gdb_message(payload_pos)}) {
@@ -127,6 +203,11 @@ int main()
                 send_msg("");
                 gdb_ok_msg(read_pos);
             }
+            std::cout << "memory content: " << std::hex;
+            for (size_t i{0}; i < 10; ++i) {
+              std::cout << std::setw(2) << std::setfill('0') << unsigned(mem[i]) << " ";
+            }
+            std::cout << '\n';
         }
         std::cout << "[main] Read error\n";
     });
